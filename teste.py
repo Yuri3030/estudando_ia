b9f056f3389e87089ae42264d9fbd05460042492
Yuri Santana Oliveira <yuriyuri@academico.ufs.br>
2026-01-15 08:34:37 -0300

    subindo arquivos de estudo

---
+++ teste.py
@@ -0,0 +1,8 @@
+from typing import Any, Callable, Iterable, Optional, List, Tuple, Dict, Set
+import heapq              # fila de prioridade (UCS / A*)
+from collections import deque  # fila (BFS)
+import math               # inf, etc.
+import random
+
+
+
